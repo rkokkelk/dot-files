472548039fa15b72fa46c89a29b517c3a6a3da9f
Roy Kokkelkoren <roy.kokkelkoren@gmail.com>
2016-10-17 16:17:51 +0200

    Added laptop specific branch

--- .tmux/default.sh
+++ .tmux/default.sh
@@ -2,7 +2,7 @@
 tmux new-session -d 'zsh'
 tmux new-window -t 7 'mc'
 tmux new-window -t 8 'htop'
-tmux new-window -t 9 -n 'logs' 'tail -f /var/log/messages.log | ccze'
-tmux split-window -v -t 9 -p 50 'tail -f /var/log/messages.log | ccze'
+tmux new-window -t 9 -n 'logs' 'tail -f -n 50 /var/log/auth.log | ccze'
+tmux split-window -v -t 9 -p 50 'tail -f -n 50 /var/log/syslog | ccze'
 tmux select-window -t 1 
 tmux -2 attach-session -d 
