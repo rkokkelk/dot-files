ff7a2ff20aeab992767d59bc11dca2af35750453
Roy Kokkelkoren <roy.kokkelkoren@gmail.com>
2017-01-06 11:11:24 +0100

    Added if for number of arguments given to tmux cmd

--- .tmux/default.sh
+++ .tmux/default.sh
@@ -1,8 +1,14 @@
 #!/bin/sh 
-tmux new-session -d 'zsh'
-tmux new-window -t 7 'mc'
-tmux new-window -t 8 'htop'
-tmux new-window -t 9 -n 'logs' 'tail -f -n 50 /var/log/auth.log | ccze'
-tmux split-window -v -t 9 -p 50 'tail -f -n 50 /var/log/syslog | ccze'
-tmux select-window -t 1 
-tmux -2 attach-session -d 
+
+# Verify if arguments are given
+if [ $# -gt 0 ]; then
+  tmux $@
+else
+  tmux new-session -d 'zsh'
+  tmux new-window -t 7 'mc'
+  tmux new-window -t 8 'htop'
+  tmux new-window -t 9 -n 'logs' 'tail -f -n 50 /var/log/auth.log | ccze'
+  tmux split-window -v -t 9 -p 50 'tail -f -n 50 /var/log/syslog | ccze'
+  tmux select-window -t 1 
+  tmux -2 attach-session -d 
+fi
