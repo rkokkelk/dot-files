306664a42b0334d420c64fc78e7b89ed41d1702b
Roy Kokkelkoren <roy.kokkelkoren@gmail.com>
2017-08-25 16:41:06 +0200

    Added -A argument to tail logs into ccze

--- .tmux/default.sh
+++ .tmux/default.sh
@@ -8,8 +8,8 @@ else
   tmux new-window -t 6 'irssi'
   tmux new-window -t 7 'mc'
   tmux new-window -t 8 'htop'
-  tmux new-window -t 9 -n 'logs' 'tail -f -n 50 /var/log/auth.log | ccze'
-  tmux split-window -v -t 9 -p 50 'tail -f -n 50 /var/log/syslog | ccze'
+  tmux new-window -t 9 -n 'logs' 'tail -f -n 50 /var/log/auth.log | ccze -A'
+  tmux split-window -v -t 9 -p 50 'tail -f -n 50 /var/log/syslog | ccze -A'
   tmux select-window -t 1 
   tmux -2 attach-session -d 
 fi
